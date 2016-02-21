fa65c0ed0b43c0aa2d66500db4afd72ed0bcaa15
ezequiel <ezequielbrrt@gmailcom>
2016-02-20 20:05:33 -0600

    Conversion ieee

---
+++ conversion.py
@@ -0,0 +1,14 @@
+#! /bin/python
+# -*- coding: utf-8 -*-
+
+def convertirBin_Dec():
+	pass
+
+def convertirDec_Bin():
+	pass
+
+def main():
+	numflot = float(raw_input("Escribe el numero: "))
+	numBin = raw_input("Escribe el numero binario: ")
+
+main()
