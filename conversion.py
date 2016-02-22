c138423cf4dbb63a69696176ddf8a54710a18c16
ezequiel <ezequielbrrt@gmailcom>
2016-02-21 21:26:14 -0600

    Actualización del Archivo

--- conversion.py
+++ conversion.py
@@ -1,14 +1,53 @@
 #! /bin/python
 # -*- coding: utf-8 -*-
 
-def convertirBin_Dec():
-	pass
+import struct
 
-def convertirDec_Bin():
-	pass
+def convertirBin_Dec(numero):
+	return numero
+
+def convertirDec_Bin(numero):
+	numConv=[]
+	if numero > 0:
+		numConv.append("0") 
+	else:
+		numConv.append("1")
+	signo = numero.split("-")
+	if len(signo) > 1:#tiene signo
+		num = signo[1].split(".")
+	else:
+		num = signo[0].split(".")
+	numeroBinario = bin(int(num[0]))
+	numeroBinario = numeroBinario[2:]
+	decimal = num[1]
+	print decimal
+	print numeroBinario
+	return numConv
+	
+def binary(num):
+    return ''.join(bin(ord(c)).replace('0b', '').rjust(8, '0') for c in struct.pack('!f', num))
 
 def main():
-	numflot = float(raw_input("Escribe el numero: "))
-	numBin = raw_input("Escribe el numero binario: ")
+	x = True
+	while x:
+		print("Selecciona una opción: ")
+		opcion = raw_input("1. Decimal a binario \n2. Binario a Decimal"+
+			"\nPresione otra tecla para salir.\n")
+		if opcion == "1":
+			try:
+				numflot = float(raw_input("Escribe el numero: "))
+			except :
+				print "Error"
+				break
+			print convertirDec_Bin(str(numflot))
+		elif opcion == "2":
+			try:
+				numBin = raw_input("Escribe el numero binario: ")
+			except:
+				print "Error"
+				break
+			print convertirBin_Dec(numBin)
+		else:
+			x = False
 
 main()
